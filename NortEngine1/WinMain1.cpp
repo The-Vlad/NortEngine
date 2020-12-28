1c0dfe103b8f60788a40a04d5be265ad95fe9b7d
The-Vlad <36847747+The-Vlad@users.noreply.github.com>
2020-12-28 15:07:42 +0300

    sync

--- NortEngine1/WinMain1.cpp
+++ NortEngine1/WinMain1.cpp
@@ -1,46 +1,5 @@
-#include <Windows.h>
-#include "WindowsMessageMap.h"
-#include <sstream>
+#include "Window.h"
 
-LRESULT CALLBACK WndProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam ) {		// Window Processor
-	static WindowsMessageMap mm;
-	OutputDebugString( mm( msg, lParam, wParam ).c_str() );
-
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage( 69 );
-		break;
-	case WM_KEYDOWN:
-		if (wParam == 'F')
-		{
-			SetWindowText( hWnd, "Respects" );
-		}
-		break;
-	case WM_KEYUP:
-		if (wParam == 'F')
-		{
-			SetWindowText( hWnd, "Dangerfield" );
-		}
-		break;
-	case WM_CHAR:
-	{
-		static std::string title;
-		title.push_back( (char)wParam );
-		SetWindowText( hWnd, title.c_str() );
-		break;
-	}
-	case WM_LBUTTONDOWN:
-	{
-		const POINTS pt = MAKEPOINTS( lParam );
-		std::ostringstream oss;
-		oss << "(" << pt.x << "," << pt.y << ")";
-		SetWindowText( hWnd, oss.str().c_str() );
-		break;
-	}
-	}
-	return DefWindowProc( hWnd, msg, wParam, lParam );
-}
 
 int CALLBACK WinMain(
 	HINSTANCE	hInstance,
@@ -48,50 +7,20 @@ int CALLBACK WinMain(
 	LPSTR		lpCmdLine,
 	int			nCmdShow )
 {
-	const auto pClassName = "hw3dbutts";	// hard ware 3D butts
-
-	// register window class (extended) [may be creating own type of class to use it later. This class is not C++ class?]
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof( wc );
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-	RegisterClassEx( &wc );
-
-	// create window		[here is creating the instance of the own created class]
-	HWND hWnd = CreateWindowEx(
-		0, pClassName,
-		"Happy Hard Window",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
-		nullptr, nullptr, hInstance, nullptr
-	);
-
-	// show window			[use own created instance of the own created class]
-	ShowWindow( hWnd, SW_SHOW );
+	Window wnd( 800, 300, "Donkey Fart Box" );
 
 	// message pump
 	MSG msg;
 	BOOL gResult;
 	while ((gResult = GetMessage( &msg, nullptr, 0, 0 )) > 0) {
 		TranslateMessage( &msg );		// need for WM_CHAR messages
-		DispatchMessageW( &msg );
+		DispatchMessage( &msg );
 	}
 
 	if (gResult == -1)
 	{
 		return -1;
 	}
-	else
-	{
-		return msg.wParam;
-	}
+	
+	return msg.wParam;
 }
